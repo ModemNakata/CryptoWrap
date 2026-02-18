501b9ad8787b677fae2dd779c75427141519e6cf
Nakata Modem <nakatamodem@i2pmail.org>
2026-02-18 12:37:16 +0000

    update

--- app/src/routes/dashboard.rs
+++ app/src/routes/dashboard.rs
@@ -10,6 +10,22 @@ async fn dashboard() -> DashboardTemplate {
     DashboardTemplate {}
 }
 
-pub fn router() -> Router {
-    Router::new().route("/", get(dashboard))
+#[derive(Template, WebTemplate)]
+#[template(path = "auth.html")]
+struct AuthTemplate {}
+
+async fn auth() -> AuthTemplate {
+    AuthTemplate {}
 }
+
+pub fn router() -> Router {
+    Router::new()
+        .route("/", get(auth))
+        .route("/dashboard", get(dashboard))
+}
+
+// check cookie with encrypted bearer token here
+// if exists - check user - if valid - let to dashboard
+// - if not - back to /, with optional clearing cookie
+//
+// add token encryption in auth.html for new logins
