7b602b1c5551e49bd5758843aa38b24ae55d2097
Nakata Modem <nakatamodem@i2pmail.org>
2026-02-18 00:38:39 +0000

    base axum router with pretty logging

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::Html, routing::get};
+use tower_http::trace::TraceLayer;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .pretty()
+        .init();
+
+    // build our application with a single route
+    let app = Router::new().route("/", get(handler).layer(TraceLayer::new_for_http()));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    tracing::info!("Serving!");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<img alt=supertest>")
 }
