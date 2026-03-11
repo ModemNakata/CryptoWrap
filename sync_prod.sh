8cbe55380e697fa8eb0e5f3a822627065d781963
N M <n1337m@atomicmail.io>
2026-03-12 01:29:45 +0300

    prod tools

---
+++ sync_prod.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+scp -r scripts/ cw:/cryptowrap/
+scp -r app/assets/ cw:/cryptowrap/app/
+
+# restart docker container `main-cw` to apply
+scp app/target/release/app cw:/cryptowrap/app/release-cw
+# restart nginx to apply
+scp nginx_default.conf cw:/etc/nginx/conf.d/default.conf  
+
+scp docker-compose.yml cw:/cryptowrap/.
+scp docker-compose.blockchain.yml cw:/cryptowrap/.
+
+# one time only | or copy .env_example and fill the variables
+# scp .env cw:/cryptowrap/app/.
+
+# one time only or it will break synced wallet files.
+# scp -r crypto/ cw:/cryptowrap/
