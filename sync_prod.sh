4d95bb66c31071c103fed13550790df8b684bcec
Nakata Modem <nakatamodem@e.cc>
2026-03-31 00:09:02 +0200

    update

--- sync_prod.sh
+++ sync_prod.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-scp -r scripts/ cw:/cryptowrap/
-scp -r app/assets/ cw:/cryptowrap/app/
+scp -r scripts/ spb:/cryptowrap/
+scp -r app/assets/ spb:/cryptowrap/app/
 
 # restart docker container `main-cw` to apply
-scp app/target/release/app cw:/cryptowrap/app/release-cw
+scp app/target/release/app spb:/cryptowrap/app/release-cw
 # restart nginx to apply
-# scp nginx_default.conf cw:/etc/nginx/conf.d/default.conf
+# scp nginx_default.conf spb:/etc/nginx/conf.d/default.conf
 # ssl applied, so don't copy
 
-# scp docker-compose.yml cw:/cryptowrap/.
-# scp docker-compose.blockchain.yml cw:/cryptowrap/.
+# scp docker-compose.yml spb:/cryptowrap/.
+# scp docker-compose.blockchain.yml spb:/cryptowrap/.
 
 # one time only | or copy .env_example and fill the variables
-# scp .env cw:/cryptowrap/app/.
+# scp .env spb:/cryptowrap/app/.
 
 # one time only or it will break synced wallet files.
-# scp -r crypto/ cw:/cryptowrap/
+# scp -r crypto/ spb:/cryptowrap/
